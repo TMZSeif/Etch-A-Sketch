6358838c74108a84ec9c51f704e5c662def81cd4
Seif Tamer <seiftamer.faresnegm@gmail.com>
2021-04-12 20:44:45 +0300

    Added reset and change pixel amount buttons

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ for (let i = 1; i < f.length; i++) {
       "#" + Math.floor(Math.random() * 16777215).toString(16);
   });
 }
+
+let size;
+let button = document.querySelector("#change");
+
+button.addEventListener("click", () => {
+  do {
+    size = prompt("Size: ");
+  } while (parseInt(size) > 64 || size === "" || isNaN(size));
+
+  for (let i = 1; i < f.length; i++) {
+    f[i].style.backgroundColor = "white";
+  }
+
+  container.textContent = "";
+
+  for (let i = 0; i < size * size; i++) {
+    div = document.createElement("div");
+    div.style.border = "2px solid black";
+    div.style.padding = "10%";
+    container.appendChild(div);
+  }
+
+  f = document.querySelectorAll("div");
+
+  for (let i = 1; i < f.length; i++) {
+    f[i].addEventListener("mouseenter", () => {
+      f[i].style.backgroundColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    });
+  }
+
+  container.style.gridTemplateColumns = "repeat(" + size.toString() + ", auto)";
+});
+
+let change = document.querySelector("#reset");
+
+change.addEventListener("click", () => {
+  for (let i = 1; i < f.length; i++) {
+    f[i].style.backgroundColor = "white";
+  }
+});
